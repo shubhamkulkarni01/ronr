94efe3eaf7857427c2d5e5c50cf6f6d0f09809f2
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-06-19 16:34:31 -0700

    setting up client directories

---
+++ client/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Router from './containers/Router';
+
+import './styles/index.css';
+
+ReactDOM.render(<Router />, document.getElementById('root'));
